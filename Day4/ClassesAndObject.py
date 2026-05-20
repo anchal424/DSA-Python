5b446e3c8114f21def9706e28d1ecff9a560d5a9
anchal424 <kushwahaanchal01@gmail.com>
2026-05-20 17:13:00 +0530

    Added Day4 Codes

---
+++ Day4/ClassesAndObject.py
@@ -0,0 +1,6 @@
+class Student:
+    def show(self):
+        print("This is a student class")
+
+s=Student();
+s.show();
---
+++ Day4/Constructor.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self):
+        print("Default constructor")
+
+    def show(self):
+        print("This is a student class")
+
+s=Student();
+s.show();
